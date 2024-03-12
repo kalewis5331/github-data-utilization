75b2fd655a724fd60944bee5e816323669973c59
kalewis5331 <kalewis5331@gmail.com>
2024-03-12 00:54:15 -0400

    Created Occurrences of Repo Languages

--- controller/repo_lang_controller.py
+++ controller/repo_lang_controller.py
@@ -1,21 +1,18 @@
 import httpx
+import numpy as np
 from fastapi import APIRouter
+import pandas as pd
+import matplotlib.pyplot as plt
 
 router = APIRouter(prefix="/languages", tags=["language", "github"])
 
 
 @router.get("/")
-async def get_languages():
+async def languages_chart():
     query = """
   {
     repos {
-      id
       language
-      name
-      description
-      stargazers_count
-      forks
-      html_url
     }
   }
     """
@@ -25,6 +22,19 @@ async def get_languages():
     if status == 200:
         data = r.json()["data"]
         print(data["repos"])
+        df = pd.DataFrame(data["repos"])
+        df_summed = df.groupby('language').size().reset_index(name='count')
+        df_summed['count'] = df_summed['count']
+        plt.bar(df_summed['language'], df_summed['count'])
+        plt.xlabel('Language')
+        plt.ylabel('Count')
+        plt.title('Occurrences of Repository Languages')
+        plt.yticks(np.arange(min(df_summed['count']), max(df_summed['count'])+1, 1.0))
+
+        # Show plot
+        plt.show()
+        plt.savefig('./static/images/new_plot.png')
+
         return data["repos"]
     else:
         r.raise_for_status()
